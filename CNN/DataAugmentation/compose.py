fbe6d20ff4eb2a245b654cb19bb43d4edbd70478
Jemelrey Abastillas <jemelreyabastillas@gmail.com>
2026-08-01 22:12:21 +0800

    Add data augmentation transforms for training and testing

---
+++ CNN/DataAugmentation/compose.py
@@ -0,0 +1,14 @@
+import torchvision.transforms as transforms
+
+train_transform = transforms.Compose([
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(15),
+    transforms.RandomCrop(32, padding=4),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+test_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
