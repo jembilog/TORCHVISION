5766c5bbb8adff26cbc7c39106e9740a012d7e74
Jemelrey Abastillas <jemelreyabastillas@gmail.com>
2026-08-02 21:44:28 +0800

    Enhance CIFAR10 CNN model with data augmentation

--- CNN/CIFAR10-CNN/model.py
+++ CNN/CIFAR10-CNN/model.py
@@ -1,55 +1,133 @@
 import torch
 import torch.nn as nn
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader, random_split
+from sklearn.metrics import accuracy_score,confusion_matrix, classification_report
+import matplotlib.pyplot as plt
+import numpy as np
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+classes = (
+    "Airplane",
+    "Automobile",
+    "Bird",
+    "Cat",
+    "Deer",
+    "Dog",
+    "Frog",
+    "Horse",
+    "Ship",
+    "Truck"
+)
+
+#data augmentation and normalization
+train_transform = transforms.Compose([
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(15),
+    transforms.RandomCrop(32,padding=4),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        (0.4914,0.4822,0.4465),
+        (0.2023,0.1994,0.2010)
+    )
+])
+
+test_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(
+        (0.4914,0.4822,0.4465),
+        (0.2023,0.1994,0.2010)
+    )
+])
+
+train_dataset = datasets.CIFAR10(
+    root="/data",
+    train=True,
+    download=True,
+    transform=train_transform
+)
+
+test_dataset = datasets.CIFAR10(
+    root="/data",
+    train=False,
+    download=True,
+    transform=test_transform
+)
+
+train_size = int(0.8 * len(train_dataset))
+val_size = len(train_dataset) - train_size
+
+train_dataset, val_dataset= random_split(
+    train_dataset,[train_size,val_size]
+)
+
+train_loader = DataLoader(train_dataset, batch_size=64,shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=64,shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=64,shuffle=False)
+
 
 class CIFARCNN(nn.Module):
     def __init__(self):
         super().__init__()
+        #block 1
         self.conv1 = nn.Conv2d(
-            in_channels = 3,
-            out_channels = 32,
-            kernel_size = 3,
-            stride = 1,
-            padding = 1
+            in_channels=3,
+            out_channels=32,
+            kernel_size=3,
+            padding=1
         )
+        self.bn1 = nn.BatchNorm2d(32)
         self.relu1 = nn.ReLU()
-        self.pool1 = nn.MaxPool2d(kernel_size = 2, stride = 2)
-        self.conv2 = nn.Conv2d(
-            in_channels = 32,
-            out_channels = 64,
-            kernel_size = 3,
-            stride = 1,
-            padding = 1
-        )
-        self.relu2 = nn.ReLU()
-        self.pool2 = nn.MaxPool2d(kernel_size = 2, stride = 2)
-        self.conv3 = nn.Conv2d(
-            in_channels = 64,
-            out_channels = 128,
-            kernel_size = 3,
-            stride = 1,
-            padding = 1
-        )
-        self.relu3 = nn.ReLU()
-        self.pool3 = nn.MaxPool2d(kernel_size = 2, stride = 2)
-        self.flatten = nn.Flatten() 
-        self.fc1 = nn.Linear(128 * 4 * 4, 512)
-        self.relu4 = nn.ReLU()
-        self.dropout = nn.Dropout(0.5) 
-        self.fc2 = nn.Linear(512, 10)
+        self.pool1 = nn.MaxPool2d(kernel_size=2,stride=2)
 
+        #block 2
+        self.conv2 = nn.Conv2d(
+            in_channels=32,
+            out_channels=64,
+            kernel_size=3,
+            padding=1
+        )
+        self.bn2 = nn.BatchNorm2d(64)
+        self.relu2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(kernel_size=2,stride=2)
+
+        #block 3
+        self.conv3 = nn.Conv2d(
+            in_channels=64,
+            out_channels=128,
+            kernel_size=3,
+            padding=1
+        )
+        self.bn3 = nn.BatchNorm2d(128)
+        self.relu3 = nn.ReLU()
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(2048, 512)
+        self.relu4 = nn.ReLU()
+        self.dropout = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(512,10)
     def forward(self, x):
         x = self.conv1(x)
+        x = self.bn1(x)
         x = self.relu1(x)
         x = self.pool1(x)
+
         x = self.conv2(x)
+        x = self.bn2(x)
         x = self.relu2(x)
         x = self.pool2(x)
+
         x = self.conv3(x)
+        x = self.bn3(x)
         x = self.relu3(x)
         x = self.pool3(x)
+
         x = self.flatten(x)
+
         x = self.fc1(x)
         x = self.relu4(x)
         x = self.dropout(x)
+
         x = self.fc2(x)
         return x
