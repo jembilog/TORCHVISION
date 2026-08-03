44273e50d338d86c561bf1d568358944848568b3
Jemelrey Abastillas <jemelreyabastillas@gmail.com>
2026-08-03 20:30:11 +0800

    Enhance CIFAR10 CNN model and training process
    Refactor CIFAR10 CNN model for improved performance and add validation set.

--- CNN/CIFAR10-CNN/model.py
+++ CNN/CIFAR10-CNN/model.py
@@ -1,12 +1,13 @@
 import torch
 import torch.nn as nn
 from torchvision import datasets, transforms
-from torch.utils.data import DataLoader, random_split
+from torch.utils.data import DataLoader, random_split, Subset
 from sklearn.metrics import accuracy_score,confusion_matrix, classification_report
 import matplotlib.pyplot as plt
 import numpy as np
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(torch.cuda.is_available())
 classes = (
     "Airplane",
     "Automobile",
@@ -32,7 +33,7 @@ train_transform = transforms.Compose([
     )
 ])
 
-test_transform = transforms.Compose([
+val_test_transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize(
         (0.4914,0.4822,0.4465),
@@ -40,31 +41,38 @@ test_transform = transforms.Compose([
     )
 ])
 
-train_dataset = datasets.CIFAR10(
+base_train_dataset = datasets.CIFAR10(
     root="/data",
     train=True,
     download=True,
     transform=train_transform
 )
-
+base_val_dataset = datasets.CIFAR10(
+    root="/data",
+    train=True,
+    download=True,
+    transform=val_test_transform
+)
 test_dataset = datasets.CIFAR10(
     root="/data",
     train=False,
     download=True,
-    transform=test_transform
+    transform=val_test_transform
 )
 
-train_size = int(0.8 * len(train_dataset))
-val_size = len(train_dataset) - train_size
+#split
+indices = list(range(len(base_train_dataset)))
+train_size = int(0.8 * len(base_train_dataset))
+g = torch.Generator().manual_seed(42)
+train_index , val_index = random_split(indices, [train_size, len(base_train_dataset) - train_size], generator=g)
 
-train_dataset, val_dataset= random_split(
-    train_dataset,[train_size,val_size]
-)
+train_dataset = Subset(base_train_dataset, train_index)
+val_dataset = Subset(base_val_dataset, val_index)
 
-train_loader = DataLoader(train_dataset, batch_size=64,shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=64,shuffle=False)
-test_loader = DataLoader(test_dataset, batch_size=64,shuffle=False)
-
+BATCH_SIZE = 128
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, pin_memory=True)
+val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False, pin_memory=True)
+test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False, pin_memory=True)
 
 class CIFARCNN(nn.Module):
     def __init__(self):
@@ -72,38 +80,38 @@ class CIFARCNN(nn.Module):
         #block 1
         self.conv1 = nn.Conv2d(
             in_channels=3,
-            out_channels=32,
+            out_channels=64,
             kernel_size=3,
             padding=1
         )
-        self.bn1 = nn.BatchNorm2d(32)
+        self.bn1 = nn.BatchNorm2d(64)
         self.relu1 = nn.ReLU()
         self.pool1 = nn.MaxPool2d(kernel_size=2,stride=2)
 
         #block 2
         self.conv2 = nn.Conv2d(
-            in_channels=32,
-            out_channels=64,
-            kernel_size=3,
-            padding=1
-        )
-        self.bn2 = nn.BatchNorm2d(64)
-        self.relu2 = nn.ReLU()
-        self.pool2 = nn.MaxPool2d(kernel_size=2,stride=2)
-
-        #block 3
-        self.conv3 = nn.Conv2d(
             in_channels=64,
             out_channels=128,
             kernel_size=3,
             padding=1
         )
-        self.bn3 = nn.BatchNorm2d(128)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.relu2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(kernel_size=2,stride=2)
+
+        #block 3
+        self.conv3 = nn.Conv2d(
+            in_channels=128,
+            out_channels=256,
+            kernel_size=3,
+            padding=1
+        )
+        self.bn3 = nn.BatchNorm2d(256)
         self.relu3 = nn.ReLU()
         self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
 
         self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(2048, 512)
+        self.fc1 = nn.Linear(256 * 4 * 4, 512)
         self.relu4 = nn.ReLU()
         self.dropout = nn.Dropout(0.5)
         self.fc2 = nn.Linear(512,10)
@@ -131,3 +139,70 @@ class CIFARCNN(nn.Module):
 
         x = self.fc2(x)
         return x
+
+model = CIFARCNN().to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(),lr=0.001, weight_decay=1e-4)
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=3)
+best_loss = float("inf")
+patience = 10
+counter = 0
+epochs = 100
+
+for epoch in range(epochs):
+    #training
+    model.train()
+    running_loss = 0
+    correct = 0
+    total = 0
+
+    for images, labels in train_loader:
+        images, labels = images.to(device), labels.to(device)
+        outputs= model(images)
+        loss = criterion(outputs, labels)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+        predicted = torch.argmax(outputs, dim=1)
+        correct += (predicted == labels).sum().item()
+        total += labels.size(0)
+    train_loss = running_loss / len(train_loader)
+    train_accuracy = correct / total
+
+    #evaluation of the m,odel inside the training loop 
+    model.eval()
+    val_loss = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for images, labels in val_loader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            val_loss += loss.item()
+            predicted = torch.argmax(outputs, dim=1)
+            correct += (predicted == labels).sum().item()
+            total += labels.size(0)
+    val_loss = val_loss / len(val_loader)
+    val_accuracy = correct / total
+    current_lr = optimizer.param_groups[0]['lr']
+    scheduler.step(val_loss)
+    print(
+        f"Epoch {epoch+1} | "
+        f"Train Loss: {train_loss:.4f} | "
+        f"Train Acc: {train_accuracy*100:.2f}% | "
+        f"Val loss: {val_loss:.4f} | " 
+        f"Val Acc: {val_accuracy*100:.2f}%"
+    )
+
+    if val_loss < best_loss:
+        best_loss = val_loss
+        counter = 0
+        torch.save(model.state_dict(),"best_model.pth")
+        print("Model saved")
+    else:
+        counter += 1
+        if counter >= patience:
+            print("Early stopping triggered")
+            break
