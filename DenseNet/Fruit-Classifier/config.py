2559baf7c2d8c2f21d19c66320a6d56568fe1bb3
Jemelrey Abastillas <jemelreyabastillas@gmail.com>
2026-08-06 22:26:49 +0800

    Add configuration for training and model settings
    Added configuration settings for dataset paths, image size, training parameters, model details, and device settings.

---
+++ DenseNet/Fruit-Classifier/config.py
@@ -0,0 +1,35 @@
+#for dataset
+TRAIN_DIR = "dataset/train"
+VAL_DIR = "dataset/validation"
+TEST_DIR = "dataset/test"
+
+#image settings
+IMAGE_SIZE = 224
+
+#training
+BATCH_SIZE = 64
+EPOCHS = 20
+LEARNING_RATE = 0.001
+
+#model
+NUM_CLASSES = 9
+
+#save model
+MODEL_PATH = "best_model.pth"
+
+#device
+DEVICE = "cuda"
+
+SEED = 42
+
+WEIGHT_DECAY = 1e-4
+
+NUM_WORKERS = 4
+
+PIN_MEMORY = True
+
+DROPOUT = 0.5
+
+SAVE_BEST = True
+
+PATIENCE = 5
